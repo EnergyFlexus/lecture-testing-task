507ab822ff3a596dd3f3567217197d354c1e2b9c
Alexey <aleksei.nagaev@onlyoffice.com>
2024-12-03 03:07:02 +0300

    Fix bug & add tests

--- 05_hw/test/tests/test_smoke.cpp
+++ 05_hw/test/tests/test_smoke.cpp
@@ -28,3 +28,34 @@ TEST(SmokeStack, TestEmpty)
 
 	destroyStack(&stack);
 }
+TEST(SmokeStack, TestNoEmpty)
+{
+	// Arrange
+	Stack stack;
+	initStack(&stack);
+
+	// Act
+	push(&stack, 1);
+	bool is_empty = isEmpty(&stack);
+
+	// Assert
+	EXPECT_EQ(is_empty, false);
+
+	destroyStack(&stack);
+}
+
+TEST(SmokeStack, TestDestroyNoEmpty)
+{
+	// Arrange
+	Stack stack;
+	initStack(&stack);
+	push(&stack, 1);
+	push(&stack, 2);
+
+	// Act
+	destroyStack(&stack);
+
+	// Assert
+	SUCCEED();
+}
+
--- 05_hw/test/tests/test_stack.cpp
+++ 05_hw/test/tests/test_stack.cpp
@@ -1,12 +1,143 @@
 #include <gtest/gtest.h>
+#include <sstream>
+#include <iostream>
 
-TEST(TestCaseName, TestName)
+#include "../../src/stack.h"
+
+TEST(TestStack, TestPush)
 {
-	EXPECT_EQ(1, 1);
-	EXPECT_TRUE(true);
+	// Arrange
+	Stack stack;
+	initStack(&stack);
+
+	// Act
+	push(&stack, 1);
+	push(&stack, 2);
+
+	// Assert
+	int current = stack.top->data;
+	int current_next = stack.top->next->data;
+	EXPECT_EQ(current, 2);
+	EXPECT_EQ(current_next, 1);
+
+	destroyStack(&stack);
 }
-TEST(TestCaseName2, TestName2)
+
+TEST(TestStack, TestPop)
 {
-	EXPECT_EQ(1, 1);
-	EXPECT_TRUE(true);
+	// Arrange
+	Stack stack;
+	initStack(&stack);
+
+	// Act
+	push(&stack, 1);
+	push(&stack, 2);
+	pop(&stack);
+
+	// Assert
+	int current = stack.top->data;
+	Node* current_next = stack.top->next;
+	EXPECT_EQ(current, 1);
+	EXPECT_EQ(current_next, nullptr);
+
+	destroyStack(&stack);
 }
+
+TEST(TestStack, TestGetTop)
+{
+	// Arrange
+	Stack stack;
+	initStack(&stack);
+	push(&stack, 1);
+	push(&stack, 2);
+
+	// Act
+	int current_top = getTop(&stack)->data;
+
+	// Assert
+	EXPECT_EQ(current_top, 2);
+
+	destroyStack(&stack);
+}
+
+TEST(TestStack, TestPushPop)
+{
+	// Arrange
+	Stack stack;
+	initStack(&stack);
+
+	// Act
+	push(&stack, 1);
+	pop(&stack);
+
+	// Assert
+	bool is_empty = isEmpty(&stack);
+	EXPECT_EQ(is_empty, true);
+
+	destroyStack(&stack);
+}
+
+TEST(TestStack, TestSearchByValue)
+{
+	// Arrange
+	Stack stack;
+	initStack(&stack);
+	push(&stack, 1);
+	push(&stack, -2);
+	push(&stack, 3);
+
+	// Act
+	Node* current_found = searchByValue(&stack, -2);
+	Node* current_not_found = searchByValue(&stack, 4);
+
+	// Assert
+	EXPECT_EQ(current_found, stack.top->next);
+	EXPECT_EQ(current_not_found, nullptr);
+
+	destroyStack(&stack);
+}
+TEST(TestStack, TestSearchByIndex)
+{
+	// Arrange
+	Stack stack;
+	initStack(&stack);
+	push(&stack, 1);
+	push(&stack, -2);
+	push(&stack, 3);
+
+	// Act
+	Node* current_found = searchByIndex(&stack, 1);
+	Node* current_not_found = searchByIndex(&stack, 3);
+
+	// Assert
+	EXPECT_EQ(current_found, stack.top->next);
+	EXPECT_EQ(current_not_found, nullptr);
+
+	destroyStack(&stack);
+}
+
+TEST(TestStack, TestTraverseStack)
+{
+	testing::internal::CaptureStdout();
+
+	// Arrange
+	Stack stack;
+	initStack(&stack);
+	push(&stack, 1);
+	push(&stack, -2);
+	push(&stack, 3);
+	push(&stack, -4);
+	push(&stack, 0);
+
+	// Act
+	traverseStack(&stack);
+
+	// Assert
+	std::string expected = "Stack elements: 0 -4 3 -2 1 \n";
+	std::string output = testing::internal::GetCapturedStdout();
+	EXPECT_EQ(expected, output);
+
+	destroyStack(&stack);
+}
+
+
